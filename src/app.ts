18a85d98fd80d87cc00045dc66c673c025dfe35a
andres <velasco.andrs@gmail.com>
2021-06-16 22:56:44 -0500

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+
 import config from 'config';
+import cors from 'cors';
+
 
 import log from './logger';
 import routes from './routes';
@@ -11,6 +14,8 @@ const port = config.get('port') as number;
 const host = config.get('host') as string;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,20 +1,23 @@
-import { createUser, login, renewToken } from '../controllers/auth-controller';
 import { Router } from 'express';
+
 import { check } from 'express-validator';
+
 import { validateBody } from '../middlewares/validate-body';
 import { validateJWT } from '../middlewares/validate-jwt';
+import { createUser, login, renewToken } from '../controllers/auth-controller';
+
 
 
 const authRouter = Router();
 
 
 authRouter.post(
-    '/', 
+    '/',
     [
         check('email', 'The email is required').not().isEmpty(),
         check('email', 'The email is not valid').isEmail(),
         check('password', 'The password is required').not().isEmpty(),
-        check('password', 'The password must be at least 6 characters').isLength({min: 6}),
+        check('password', 'The password must be at least 6 characters').isLength({ min: 6 }),
         validateBody,
     ],
     login,
@@ -27,14 +30,14 @@ authRouter.post(
         check('email', 'The email is required').not().isEmpty(),
         check('email', 'The email is not valid').isEmail(),
         check('password', 'The password is required').not().isEmpty(),
-        check('password', 'The password must be at least 6 characters').isLength({min: 6}),
+        check('password', 'The password must be at least 6 characters').isLength({ min: 6 }),
         validateBody,
     ],
     createUser,
 );
 
 authRouter.get(
-    '/renew', 
+    '/renew',
     [
         validateJWT,
     ],
